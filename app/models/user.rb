9681836caddc38eb5705e6bdc6c843841e93e045
John Meeker <jmeeker516@gmail.com>
2013-06-06 00:40:42 -0700

    cleanup

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,6 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable#, :validatable,
-         #:token_authenticatable
-
-  #before_save :ensure_authentication_token
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email#, :password, :password_confirmation, :remember_me, :encrypted_password
