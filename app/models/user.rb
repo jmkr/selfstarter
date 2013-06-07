aa66f7147c24be9e89d8f9f87085ac7890d88d46
John Meeker <jmeeker516@gmail.com>
2013-06-07 00:50:57 -0700

    basic devise authenication added and working

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,18 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable#, :validatable,
+         :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email#, :password, :password_confirmation, :remember_me, :encrypted_password
+  attr_accessible :email, :password, :password_confirmation, :remember_me
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :confirmable#, :validatable,
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :encrypted_password
 
   has_many :orders
 end
