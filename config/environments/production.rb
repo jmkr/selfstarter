c22f4e28b80240f9bd5fa1346849795cdcf84051
John Meeker <jmeeker516@gmail.com>
2013-06-07 01:11:47 -0700

    config prod action mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,7 @@ Selfstarter::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Config action mailer url
+  config.action_mailer.default_url_options = { :host => 'craftcrate.heroku.com' }
 end
