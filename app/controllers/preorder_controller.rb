14a513effefc490b4eee5ee8a033b6cc7009c702
David Melin <davidmmelin@gmail.com>
2013-07-25 23:28:37 -0700

    Trimmed and fixed preorder controller and order model

--- app/controllers/preorder_controller.rb
+++ app/controllers/preorder_controller.rb
@@ -5,74 +5,32 @@ class PreorderController < ApplicationController
   end
 
   def checkout
-
-    if @user = current_user
-      puts "current user?? #{@user.email}"
-    end
-
+    debugger
   end
 
   def prefill
-
-    # Get the current_user if one is already sign-in. If not, create a 
-    # new user with the given parameters.
-    if current_user
-      @user = current_user
-    elsif !params[:email].blank? && !params[:password].blank? && params[:password] == params[:passwordConfirm]
-      @user  = User.find_or_create_by_email!(params[:email], :password => params[:password])
-      sign_in @user, :bypass => true 
-    else
-      raise Exception.new("Please enter an email address and valid password")
-    end
-
-    if Settings.use_payment_options
-      payment_option_id = params['payment_option']
-      raise Exception.new("No payment option was selected") if payment_option_id.nil?
-      payment_option = PaymentOption.find(payment_option_id)
-      price = payment_option.amount
-    else
-      price = Settings.price
-    end
-
-    puts "name?? #{params[:name]}"
-    puts "state?? #{params[:state ]}"
-
-
-    # Validate the Shipping data from the form.
-    if !params[:name].blank? && !params[:address1].blank? && !params[:city].blank? && !params[:state].blank? && !params[:zip].blank?
-    else
-      flash[:error] = "Please fill out all required fields"
-      redirect_to preorder_checkout_path
-      return
-    end
-
-    @order = Order.prefill!(
-      :name => Settings.product_name, 
-      :price => price, 
-      :user_id => @user.id, 
-      :payment_option => payment_option
-    )
-
-    # Get the credit card details submitted by the form
-    token = params[:stripeToken]
+ 
+    #why is name in the order the current_user's name?
+    #params[:order][:name] = Settings.product_name
+    params[:order][:price] = Settings.price
+    @order = current_user.orders.new(params[:order])
     
+    unless @order.save
+      redirect_to :action => :prefill, flash[:notice] => "Unable to save order"
+    end
+
     # Create a Customer object via Stripe.
-    @customer = Stripe::Customer.create(
+    customer = Stripe::Customer.create(
         :card => params[:stripeToken],
         :plan => "craftcrate-monthly",
-        :email => @user.email
+        :email => current_user.email
       )
 
-    unless @customer.nil?
-      Order.postfill!(@order.uuid, @customer)
+    if customer
+      @order.postfill!(customer)
+      render :share
     else
       redirect_to :action => :prefill, flash[:notice] => "Unable to authorize credit card"
-    end
-
-    if @customer.present?
-      redirect_to :action => :share, :uuid => @order.uuid
-    else
-      redirect_to root_url
     end
 
   rescue Stripe::CardError => e
@@ -80,10 +38,11 @@ class PreorderController < ApplicationController
     redirect_to root_url    
   end
 
-  def share
-    @order = Order.find_by_uuid(params[:uuid])
-  end
+  # def share
+  #   #does anyone ever go directly to share?... this is never used
+  #   @order = Order.find_by_uuid(params[:uuid])
+  # end
 
-  def ipn
-  end
+  # def ipn
+  # end
 end
