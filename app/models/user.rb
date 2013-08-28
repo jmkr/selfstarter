47624177d09e7fefcd7e143a7018c3c92f120ee2
David Melin <davidmmelin@gmail.com>
2013-08-28 14:14:41 -0700

    Fixed double error render

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,11 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :confirmable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable #:confirmable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :remember_me, :password_confirmation, :remember_me
 
-  validates :email, uniqueness: true
   has_many :orders
   has_many :shipments, through: :orders
 end
