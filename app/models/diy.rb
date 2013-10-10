88c82b8330647bb00c178cdee959f964d4f1b62f
David Melin <davidmmelin@gmail.com>
2013-10-09 22:32:22 -0700

    DIY index and show, also nav clicked link

--- app/models/diy.rb
+++ app/models/diy.rb
@@ -1,6 +1,6 @@
 #This is going to be created entirely from the admin page
 class Diy < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :title, :body, :image
 
   validates :title, :image, presence: true
 
